446feaa23b68ac0f1ccdb0c23c53dbe789cb4ced
‘pengchenyu’ <iampengchenyu@163.com>
2022-06-28 22:24:48 +0800

    初始化开发环境

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# 一、环境配置说明
+
+1. 创建虚拟环境
+
+   ```shell
+   conda create -n nilmtk-ev python=3.7
+   ```
+   
+2. 导入所需依赖
+
+   ```shell
+   # 注意需要进入requirements所在的文件夹
+    conda install --yes --file requirements.txt
+   ```
+   
+3. CSDN上的博客都是水的！直接将github上的nilmtk、nilm_metadata、nilmtk_contrib的相关文件夹复制过来即可(只需要子文件夹，不要带setup.py的那个父文件夹)。
+
+   ```shell
+   # 注意需要进入requirements所在的文件夹
+    conda install --yes --file requirements.txt
+   ```
---
+++ nilmtk/losses.py
@@ -0,0 +1,66 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error, f1_score, r2_score, mean_absolute_percentage_error
+import numpy as np
+
+
+def mae(app_gt, app_pred):
+    return mean_absolute_error(app_gt, app_pred)
+
+
+def rmse(app_gt, app_pred):
+    return mean_squared_error(app_gt, app_pred) ** (.5)
+
+
+def mse(app_gt, app_pred):
+    return mean_squared_error(app_gt, app_pred)
+
+
+def mape(app_gt, app_pred):
+    return mean_absolute_percentage_error(app_gt, app_pred)
+
+
+def f1score(app_gt, app_pred):
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp < threshold, 0, 1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp < threshold, 0, 1)
+
+    return f1_score(gt_temp, pred_temp)
+
+
+def relative_error(app_gt, app_pred):
+    constant = 1
+    numerator = np.abs(app_gt - app_pred)
+    denominator = constant + app_pred
+    return np.mean(numerator / denominator)
+
+
+def r2score(app_gt, app_pred):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html#sklearn.metrics.r2_score
+    return r2_score(app_gt, app_pred)
+
+
+def nde(app_gt, app_pred):
+    # Normalized Disaggregation Error (NDE)
+    # Inspired by http://proceedings.mlr.press/v22/zico12/zico12.pdf
+    numerator = np.sum((app_gt - app_pred) ** 2)
+    denominator = np.sum(app_gt ** 2)
+
+    return np.sqrt(numerator / denominator)
+
+
+def nep(app_gt, app_pred):
+    # 同SAE
+    # Normalized Error in Assigned Power (NEP)
+    # Inspired by https://www.springer.com/gp/book/9783030307813
+    numerator = np.sum(np.abs(app_gt - app_pred))
+    denominator = np.sum(app_gt)
+    return numerator / denominator
+
+
+def mr(app_gt, app_pred):
+    # match rate
+    # FROM: Load disaggregation technologies: real world and laboratory performance
+    numerator = np.sum(np.min([app_gt, app_pred], axis=0))
+    denominator = np.sum(np.max([app_gt, app_pred], axis=0))
+    return numerator / denominator
